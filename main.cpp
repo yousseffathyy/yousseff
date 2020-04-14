69f0a48d72526c4bebcbc20cfa12cb1033dd6172
yousseffathyy <63667251+yousseffathyy@users.noreply.github.com>
2020-04-14 19:56:24 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "matrix.h"
+#include "MatrixCalculator.h"
+using namespace std;
+
+int main()
+{
+    MatrixCalculator m;
+    m.menu();
+    return 0;
+}
